84d86045439d9415da9d09d5a2c062850d9722e3
gege <KartashovVD@yandex.ru>
2022-04-26 14:42:04 -0400

    Fixed discrepancies with the pep8 standard. Added dynamic content loading usin CBV.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,38 +1,41 @@
 from pipes import Template
-from re import template
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 # Create your views here.
 
 MENU_LINKS = {
-    'main': 'Главная',
-    'products': 'Продукты',
-    'contact' : 'Контакты',
+    "main": "Главная",
+    "products": "Продукты",
+    "contact": "Контакты",
 }
+
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = MENU_LINKS
-        context['title'] = 'Главная'
+        context["menu"] = MENU_LINKS
+        context["title"] = "Главная"
+        context["greeting"] = "yo creator!"
         return context
+
+
 class ProductPageView(TemplateView):
-    template_name = 'mainapp/products.html'
+    template_name = "mainapp/products.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = MENU_LINKS
-        context['title'] = 'Продукты'
+        context["menu"] = MENU_LINKS
+        context["title"] = "Продукты"
         return context
+
 
 class ContactsPageView(TemplateView):
-    template_name = 'mainapp/contact.html'
-    
+    template_name = "mainapp/contact.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = MENU_LINKS
-        context['title'] = 'Контакты'
+        context["menu"] = MENU_LINKS
+        context["title"] = "Контакты"
         return context
